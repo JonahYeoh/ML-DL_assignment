2a5379089376db009bf5d3546ce0fe0a01fe3df6
Jonah <47838117+JonahYeoh@users.noreply.github.com>
2021-03-03 20:53:47 +0800

    Update fundamental.py

--- my_nn_modules/conv/fundamental.py
+++ my_nn_modules/conv/fundamental.py
@@ -21,3 +21,25 @@ def _transition_layer2d(in_tensor, filters, pooling=True, pooling_method='avg'):
             return AveragePooling2D(pool_size=2,strides=2, padding='same')(conv)
         return MaxPooling2D(pool_size=2, strides=2, padding='same')(conv)
     return conv
+
+def _channel_attention(in_tensor, reduction=8):
+    w0 = Dense(in_tensor.shape[-1]//reduction, activation='relu')
+    w1 = Dense(in_tensor.shape[-1], activation='relu')
+    p_avg = GlobalAveragePooling2D()(in_tensor)
+    p_max = GlobalMaxPool2D()(in_tensor)
+    dense_avg = w1(w0(p_avg))
+    dense_max = w1(w0(p_max))
+    summation = Add()([dense_avg, dense_max])
+    return Activation('sigmoid')(summation)
+
+def _spatial_attention(in_tensor, kernels):
+    p_avg = AveragePooling2D(pool_size=(3,3), strides=(2,2), padding='same')(in_tensor)
+    p_max = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='same')(in_tensor)
+    stack = Concatenate()([p_avg, p_max])
+    conv = Conv2D(1, kernel_size=(7,7), strides=(1,1), padding='same')(stack)
+    return Activation('sigmoid')(conv)
+
+def _spatial_attention_module(in_tensor, kernels):
+    f1 = Multiply()([_channel_attention(in_tensor), in_tensor])
+    f2 = Multiply()([_spatial_attention(f1), f1])
+    return Add()([in_tensor, f2])
